a0ae3000ec093507538bccccb5571ffeb2225f7a
Emiliano Acosta <emilianoacosta18@outlook.com>
2023-06-01 04:18:34 -0300

    se modifico login

--- script/login.js
+++ script/login.js
@@ -1,14 +1,14 @@
 class User{
-    constructor(id,email,password){
-        this.id = id;
+    constructor(email,password){
+        
         this.email = email;
         this.password = password;
     }
 }
 
 const users = [
-    new User('admin','admin@gmail.com' , '1234'),
-    new User('invited','emiliano@gmail.com','123456')
+    new User('admin@gmail.com' , '1234'),
+    new User('emiliano@gmail.com','123456')
 ]
 
 let arrayUsers = JSON.parse(localStorage.getItem('arrayUsers')) || [];
@@ -19,14 +19,16 @@ const login = (event) =>{
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+
     const userFound = users.find(user => user.email  == email && user.password == password)
     if(userFound){
         const verficacion =userFound.email = email && userFound.password == password;
         if(verficacion){
 
-            console.log(userFound);
-            arrayUsers.push(userFound);
-
+            const id = new Date().getTime();
+            console.log(id);
+            arrayUsers.push(id);
+            
             localStorage.setItem('arrayUsers',JSON.stringify(arrayUsers));
             localStorage.getItem('arrayUsers');
 
@@ -34,16 +36,16 @@ const login = (event) =>{
             document.querySelector('.formularioLogin').reset();
 
             let sesionLogueada = document.createElement('button');
-            sesionLogueada.onclick = () => deslogueoSesion(userFound);
+            sesionLogueada.onclick = () => deslogueoSesion(id);
             sesionLogueada.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i>`;
             sesionLogueada.classList.add('mx-2','bg-success','text-light','my-2','p-2','rounded-2','class="SesionLogueada"', 'onclick="deslogueoSesion()"');
             const padreSesionLogueada = document.querySelector('.sesionLogueada');
             
             padreSesionLogueada.appendChild(sesionLogueada);
             document.querySelector('#buttonInicioSesion').remove();
-            document.querySelector('#buttonRegistro').remove();
-            const administracion = document.querySelector('.linkAdmin').textContent = 'Administracion';
-            administracion.onclick = () => mantenerSesion(event);
+            
+            document.querySelector('.linkAdmin').textContent = 'Administracion';
+
             // Administracion.classList.add()
 
         }else{
@@ -75,12 +77,12 @@ function alertMensaje2(mensaje){
     container.appendChild(alertMensaje);
 }
 
-function deslogueoSesion(userFound){
+function deslogueoSesion(id){
 
-    const singOutUser = document.getElementById(userFound);
-    arrayUsers = arrayUsers.filter(user => {user.id !== id});
+    const singOutUser = document.getElementById(id);
+    arrayUsers = arrayUsers.filter(user => {user.userFound !== id});
     localStorage.setItem('arrayUsers',JSON.stringify(arrayUsers));
-    singOutUser.remove(userFound);
+    singOutUser.remove(id);
     // window.location.reload(userFound);
 
 }
